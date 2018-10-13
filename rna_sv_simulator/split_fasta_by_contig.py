0c3d0f103035b1b06daa78d8d76e2f0e593b9304
Manuel Belmadani <manuel.belmadani@gmail.com>
2018-10-13 16:38:16 -0700

    PEP8 for split_fasta_by_contig.py

--- rna_sv_simulator/split_fasta_by_contig.py
+++ rna_sv_simulator/split_fasta_by_contig.py
@@ -4,7 +4,7 @@
 import os
 import sys
 
-FASTA_FILE=sys.argv[1]
+FASTA_FILE = sys.argv[1]
 
 if __name__ == "__main__":
     current_filename = None
@@ -12,14 +12,14 @@ if __name__ == "__main__":
 
     with open(FASTA_FILE, 'r') as fasta:
         for row in fasta:
-            if row[0] == ">": # Case where row is a header. Create new file.
-                current_filename = row.split(" ")[0] # Get contig name.
-                current_filename = current_filename[1:] # Remove `>`.
-                current_filename = current_filename.strip() # Strip trailling/leading spaces.
-                current_filename = current_filename + ".fa" # Add fasta file extension.
-        
+            if row[0] == ">":  # Case where row is a header. Create new file.
+                current_filename = row.split(" ")[0]  # Get contig name.
+                current_filename = current_filename[1:]  # Remove `>`.
+                current_filename = current_filename.strip()  # Strip trailling/leading spaces.
+                current_filename = current_filename + ".fa"  # Add fasta file extension.
+
                 output_file.close()
                 output_file = open(current_filename, 'w')
-                
+
             # Dump row to file.
             output_file.write(row)
